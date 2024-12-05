ce499b501e10e473d1c653c1ce89cfda1975cfca
mazhaobo <mazhaobo@hoteamsoft.com>
2024-12-05 16:52:21 +0800

    build(vite): 配置 dts 插件的入口目录
    - 在 react-renderer、react-simulator-renderer 和 renderer-core 包中
    - 设置 dts 插件的 entryRoot 为 'src/'
    - 这个改动可以提高类型定义文件的生成效率和准确性

--- packages/renderer-core/vite.config.ts
+++ packages/renderer-core/vite.config.ts
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react-swc';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [
+    react(),
+    dts({
+      entryRoot: 'src/',
+    }),
+  ],
   define: {
     'process.env': {},
   },
